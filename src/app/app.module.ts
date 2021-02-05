63ed640a0eda3db1779667c5269100fc4da2af0a
rahul <rsuryaa96@gmail.com>
2021-02-06 00:09:09 +0530

    Angular component, module, events examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserModule } from './user/user.module';
+import { ButtonExampleComponent } from './button-example/button-example.component';
+import { EventsExampleComponent } from './events-example/events-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonExampleComponent,
+    EventsExampleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
